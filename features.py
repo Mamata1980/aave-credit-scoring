54dd1adc3da52e691f96a4e42931e782258bb0ba
Mamata1980 <sahumilan941@gmail.com>
2025-07-22 17:12:50 +0530

    features.py

---
+++ features.py
@@ -0,0 +1,8 @@
+{
+  "wallet": "0x123...",
+  "transactions": [
+    {"action": "deposit", "amount": 1000, "timestamp": 1609459200},
+    {"action": "borrow", "amount": 500, "timestamp": 1609462800},
+    ...
+  ]
+}
